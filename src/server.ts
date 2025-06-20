9e21d2390bbc9b4a198bb5fbcf0d34d85779c02e
firoz.nur <firoznur5@gmail.com>
2025-06-20 21:54:14 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import { Server } from "http";
+import app from "./app";
+import mongoose from "mongoose";
+
+let server: Server;
+
+const PORT = 5000;
+
+async function main() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    console.log("connected to mongodb using mongoose");
+    server = app.listen(PORT, () => {
+      console.log(`App is listing on PORT ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
