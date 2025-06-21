b7f14fdbc6ea8a1c2cbea97143af65fef4333e8e
firoz.nur <firoznur5@gmail.com>
2025-06-21 11:29:04 +0600

    Get : Notes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, { Application, Request, Response } from "express";
 import { model, Schema } from "mongoose";
 const app: Application = express();
+app.use(express.json());
 
 const noteSchema = new Schema({
   title: { type: String, required: true, trim: true },
-  content: { type: String, default: "No content provided by user" },
+  content: { type: String, default: "No content provided by user", trim: true },
   category: {
     type: String,
     enum: ["personal", "work", "study", "other"],
@@ -22,22 +23,45 @@ const noteSchema = new Schema({
 
 const Note = model("note", noteSchema);
 
-app.post("/createNote", async (req: Request, res: Response) => {
-  const myNote = new Note({
-    title: "Learning Express",
-    // content: "I am learning mongoose",
-    publishDate: "hello publish date",
-    tags: {
-      label: "Database",
-    }
-  });
+app.post("/notes/createNote", async (req: Request, res: Response) => {
+  const body = req.body;
+  console.log("body", body);
+  const note = await Note.create(body);
 
-  await myNote.save();
+  // approach 01
+
+  // const myNote = new Note({
+  //   title: "Learning Express",
+  //   // content: "I am learning mongoose",
+  //   publishDate: "hello publish date",
+  //   tags: {
+  //     label: "Database",
+  //   }
+  // });
+
+  // await myNote.save();
 
   res.status(201).json({
     success: true,
     message: "Note created successfully",
-    note: myNote,
+    note,
+  });
+});
+
+app.get("/notes", async (req: Request, res: Response) => {
+  const notes = await Note.find();
+  res.status(200).json({
+    success: true,
+    notes,
+  });
+});
+
+app.get("/notes/:noteId", async (req: Request, res: Response) => {
+  const noteId = req.params.noteId;
+  const notes = await Note.findById(noteId);
+  res.status(200).json({
+    success: true,
+    notes,
   });
 });
 
